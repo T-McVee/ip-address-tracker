20544c9a8e6a99d3b7701f98f545c38da504cc5c
T-McVee <iam@tmcvee.com>
2021-10-23 07:31:06 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      IP Address Tracker Search for any IP address or domain IP Address Location
+      Timezone UTC {/* add offset value dynamically using the API */}
+      ISP
+      <div class="attribution">
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by <a href="#">Tim McVinish</a>.
+      </div>
     </div>
   );
 }
