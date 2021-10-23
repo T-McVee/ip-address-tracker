ece74efb2a017a0fc55d5af258ca0808fbe6b495
T-McVee <iam@tmcvee.com>
2021-10-23 09:50:15 -0700

    Adds initial styles for the Top component

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,33 +1,67 @@
 import styled from 'styled-components';
+import pattern from '../images/pattern-bg.png';
 
 const Header = styled.header`
+  position: relative;
   display: flex;
   flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
   width: 100%;
-  background-color: blue;
+  height: 200px;
+  background-image: url(${pattern});
   margin: 0;
   color: #ffffff;
+  box-shadow: 0px 4px 6px 1px #3f3f3f3d;
 `;
 
-const H1 = styled.h1``;
+const H1 = styled.h1`
+  font-weight: 400;
+`;
+
+const Input = styled.input`
+  font-size: 18px;
+  border-radius: 8px;
+  border: none;
+  width: 535px;
+  padding: 0.5rem 0.75rem;
+`;
+
+const Aside = styled.aside`
+  position: relative;
+  top: 35px;
+  width: calc(100% - 16rem);
+  height: 260px;
+  background-color: #ffffff;
+  border-radius: 14px;
+  box-shadow: 0px 4px 6px 1px #3f3f3f3d;
+`;
+
+const Ul = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  width: 100%;
+  list-style: none;
+`;
 
 export const Top = () => {
   return (
     <Header>
-      <h1>IP Address Tracker</h1>
+      <H1>IP Address Tracker</H1>
       <form action="">
-        <input type="text" placeholder="Search for any IP address or domain" />
+        <Input type="text" placeholder="Search for any IP address or domain" />
       </form>
-      <aside className="output">
-        <ul>
+      <Aside className="output">
+        <Ul>
           <li>IP Address</li>
           <li>Location</li>
           <li>
             Timezone UTC {/* add offset value dynamically using the API */}
           </li>
           <li>ISP</li>
-        </ul>
-      </aside>
+        </Ul>
+      </Aside>
     </Header>
   );
 };
